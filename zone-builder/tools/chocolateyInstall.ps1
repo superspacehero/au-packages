b0c42635ad282965b81039670a3e0472dd809730
Asher Pinson <ashpinson@gmail.com>
2021-07-11 22:05:00 -0400

    Added Zone Builder

---
+++ zone-builder/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = 'zone-builder'
+  fileType       = 'exe'
+  softwareName   = 'Zone Builder'
+
+  checksum       = 'c442402e5791dc3d91b349eeac58662f'
+  checksumType   = 'md5'
+  url            = 'https://mb.srb2.org/addons/zone-builder.149/download/'
+
+  silentArgs     = '/silent'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
